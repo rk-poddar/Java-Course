f675a32bd4635a409d2bd46a042bd0103e35416d
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-23 13:49:47 +0530

    Pattern-M program

--- Pattern_Programming/Pattern_M/Main.java
+++ Pattern_Programming/Pattern_M/Main.java
@@ -7,8 +7,8 @@ public class Main{
         
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(j==0 || i==j&&i<=(n-1)/2 
-                || i+j==(n-1)&&i<=(n-1)/2 || j==(n-1))
+                if(j==0 || i==j && i<=(n-1)/2 
+                || i+j==(n-1) && i<=(n-1)/2 || j==(n-1))
                 {
                     System.out.print("* ");
                 }
