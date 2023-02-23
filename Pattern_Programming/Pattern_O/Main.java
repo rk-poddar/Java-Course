f5ee1a4e9ded6d2fc180d0e9acd0a36f022c1cf5
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-23 15:09:56 +0530

    Pattern-O program

--- Pattern_Programming/Pattern_O/Main.java
+++ Pattern_Programming/Pattern_O/Main.java
@@ -8,10 +8,10 @@ public class Main
 		
 		for(int i=0; i<n; i++){
 		    for(int j=0; j<n; j++){
-		        if(i==0 && j>0 && j<(n-1)/2 
-		        || j==0 && i>0 && i<(n-1) 
-		        || i==(n-1) && j>0 && j<(n-1)/2
-		        || j==(n-1)/2 && i>0 && i<(n-1))
+		        if(i==0 && j>0 && j<(n-1)
+		        || j==0 && i>0 && i<(n-1)
+		        || i==(n-1) && j>0 && j<(n-1)
+		        || j==(n-1) && i>0 && i<(n-1))
 		        {
 		            System.out.print("* ");
 		        }
