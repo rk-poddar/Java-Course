f386bca220175af5e660a5386c97b0b1f6fbf6c1
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-23 13:38:55 +0530

    Pattern-L program

---
+++ Pattern_Programming/Pattern_L/Main.java
@@ -0,0 +1,20 @@
+package Pattern_L;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		int n=7;
+		for(int i=0; i<n; i++){
+		    for(int j=0; j<n; j++){
+		        if(j==0 || i==(n-1) && j<=(n-1)/2){
+		            System.out.print("* ");
+		        }
+		        else{
+		            System.out.print("  ");
+		        }
+		    }
+		    System.out.println("");
+		}
+	}
+}
