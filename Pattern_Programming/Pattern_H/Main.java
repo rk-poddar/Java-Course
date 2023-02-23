43e8a03c49ba9d2a4a04a8ef9f94ec368fc973e7
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-23 13:07:16 +0530

    Pattern-H program

---
+++ Pattern_Programming/Pattern_H/Main.java
@@ -0,0 +1,22 @@
+package Pattern_H;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		int n=7;
+		
+		for(int i=0; i<n; i++){
+		    for(int j=0; j<n; j++){
+		        if(j==0 || i==(n-1)/2&&j<(n-1)/2 
+		        || j==(n-1)/2){
+		            System.out.print("* ");
+		        }
+		        else{
+		            System.out.print("  ");
+		        }
+		    }
+		    System.out.println("");
+		}
+	}
+}
