c47a156740075eabb66f8be6316f0ca9273273bd
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-23 17:53:18 +0530

    Pattern-V program

---
+++ Pattern_Programming/Pattern_V/Main.java
@@ -0,0 +1,22 @@
+package Pattern_V;
+
+public class Main{
+    public static void main(String[] args){
+        
+        int n=7;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==j && i<=(n-1)/2 
+                || i+j==(n-1) && i<=(n-1)/2)
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
