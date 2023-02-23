7abe5d5c3c2998e00b9d4df824efec3cfeede9df
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-23 15:47:31 +0530

    Pattern-R program

---
+++ Pattern_Programming/Pattern_R/Main.java
@@ -0,0 +1,23 @@
+package Pattern_R;
+
+public class Main{
+    public static void main(String[] args){
+        
+        int n=7;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(j==0 || i==0 && j<(n-1)/2 
+                || j==(n-1)/2 && i>0 && i<(n-1)/2 
+                || i==(n-1)/2 && j<(n-1)/2 || i-j==(n-1)/2)
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
