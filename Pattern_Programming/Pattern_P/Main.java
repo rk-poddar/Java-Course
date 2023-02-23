230eb601ea5fb32e80f65c8fe0b03e03955e2a98
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-23 15:00:26 +0530

    Pattern-P program

---
+++ Pattern_Programming/Pattern_P/Main.java
@@ -0,0 +1,20 @@
+package Pattern_P;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		int n=7;
+		for(int i=0; i<n; i++){
+		    for(int j=0; j<n; j++){
+		        if(i==0&&j<(n-1)/2 || j==0 || i==(n-1)/2&&j<(n-1)/2 || j==(n-1)/2&&i>0&&i<(n-1)/2){
+		            System.out.print("* ");
+		        }
+		        else{
+		            System.out.print("  ");
+		        }
+		    }
+		    System.out.println("");
+		}
+	}
+}
