81f2bde11faf48000c36bb28046d233868d237db
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-23 17:59:38 +0530

    Pattern-Z program

---
+++ Pattern_Programming/Pattern_Z/Main.java
@@ -0,0 +1,20 @@
+package Pattern_Z;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		int n=7;
+		for(int i=0; i<n; i++){
+		    for(int j=0; j<n; j++){
+		        if(i==0 || i+j==n-1 || i==n-1){
+		            System.out.print("* ");
+		        }
+		        else{
+		            System.out.print("  ");
+		        }
+		    }
+		    System.out.println("");
+		}
+	}
+}
