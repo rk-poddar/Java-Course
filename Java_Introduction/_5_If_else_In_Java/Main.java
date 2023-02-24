608441f103fcfe91d7dc4fb205406773ab87fe34
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-24 18:44:34 +0530

    if-else & nested if-else

---
+++ Java_Introduction/_5_If_else_In_Java/Main.java
@@ -0,0 +1,33 @@
+package Java_Introduction._5_If_else_In_Java;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // If-else statement
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not an adult.");
+        }
+
+        // Nested if-else statement
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        } else if (num < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        input.close();
+    }
+}
+
