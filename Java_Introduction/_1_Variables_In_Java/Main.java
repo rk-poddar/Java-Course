5b4d76529a2bd645037c178d879e4e828f6adc36
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-24 15:40:19 +0530

    variables

---
+++ Java_Introduction/_1_Variables_In_Java/Main.java
@@ -0,0 +1,13 @@
+package Java_Introduction._1_Variables_In_Java;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // declare variables
+        int x = 10;
+        int y = 10;
+
+        // print the sum of the variables
+        System.out.println(x+y); 
+    }
+}
