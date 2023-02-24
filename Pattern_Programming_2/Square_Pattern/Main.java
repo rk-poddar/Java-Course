70419e6981a05571cc5fbfca83ea06a48772b36a
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-24 13:10:19 +0530

    Square-Pattern program

---
+++ Pattern_Programming_2/Square_Pattern/Main.java
@@ -0,0 +1,14 @@
+package Pattern_Programming_2.Square_Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        int n=5; 
+        for(int i=1; i<=n; i++){
+           for(int j=1; j<=n; j++){
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+    }
+}
