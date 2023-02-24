9f45832c9a266546421dc6639b7b3c1526ce0191
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-24 11:26:15 +0530

    Right_Triangle_Pattern program

---
+++ Pattern_Programming_2/Triangles_Pattern/Right_Triangle_Pattern/Main.java
@@ -0,0 +1,21 @@
+package Pattern_Programming_2.Triangles_Pattern.Right_Triangle_Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Method 1
+        for(int i=1; i<=7; i++){
+           String str = "* ";
+           System.out.println(str.repeat(i));
+        }
+
+        // Method 2
+        int n=7;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
