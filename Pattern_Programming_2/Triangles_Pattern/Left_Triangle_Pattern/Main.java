4cd961d6ff1947211b5666b5bd70bceb32e94519
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-24 12:04:37 +0530

    minor problem changes

--- Pattern_Programming_2/Triangles_Pattern/Left_Triangle_Pattern/Main.java
+++ Pattern_Programming_2/Triangles_Pattern/Left_Triangle_Pattern/Main.java
@@ -3,20 +3,15 @@ package Pattern_Programming_2.Triangles_Pattern.Left_Triangle_Pattern;
 public class Main {
     public static void main(String[] args) {
 
-        // Method 1
-        for(int i=1; i<=7; i++){
-            String str = "* ";
-            System.out.println(str.repeat(i));
-         }
- 
-         // Method 2
-         System.out.println(); //for line break or spacing
-         int n=7;
-         for(int i=0; i<n; i++){
-             for(int j=0; j<=i; j++){
-                 System.out.print("* ");
-             }
-             System.out.println(" ");
-         }
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
--- Pattern_Programming_2/Triangles_Pattern/Right_Triangle_Pattern/Main.java
+++ Pattern_Programming_2/Triangles_Pattern/Right_Triangle_Pattern/Main.java
@@ -3,15 +3,20 @@ package Pattern_Programming_2.Triangles_Pattern.Right_Triangle_Pattern;
 public class Main {
     public static void main(String[] args) {
 
-        int n = 7;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print("  ");
-            }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+        // Method 1
+        for(int i=1; i<=7; i++){
+            String str = "* ";
+            System.out.println(str.repeat(i));
+         }
+ 
+         // Method 2
+         System.out.println(); //for line break or spacing
+         int n=7;
+         for(int i=0; i<n; i++){
+             for(int j=0; j<=i; j++){
+                 System.out.print("* ");
+             }
+             System.out.println(" ");
+         }
     }
 }
