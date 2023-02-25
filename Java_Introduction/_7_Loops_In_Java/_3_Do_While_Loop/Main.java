6c7f2be586d479fe8a0a993836d21654b4caeb28
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-25 12:40:11 +0530

    do-while loop

---
+++ Java_Introduction/_7_Loops_In_Java/_3_Do_While_Loop/Main.java
@@ -0,0 +1,12 @@
+package Java_Introduction._7_Loops_In_Java._3_Do_While_Loop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 0;
+        do {
+            System.out.println("The value of i is " + i);
+            i++;
+        } while (i < 5);
+    }
+}
