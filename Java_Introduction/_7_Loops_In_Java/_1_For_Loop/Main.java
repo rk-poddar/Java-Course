dcddeb2563b50f1b2e3f0606d7f0c21085202c7e
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-25 12:37:45 +0530

    for loop

---
+++ Java_Introduction/_7_Loops_In_Java/_1_For_Loop/Main.java
@@ -0,0 +1,10 @@
+package Java_Introduction._7_Loops_In_Java._1_For_Loop;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("The value of i is " + i);
+        }        
+    }
+}
