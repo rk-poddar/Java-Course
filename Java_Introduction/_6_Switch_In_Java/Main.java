4553c318f525d6078db178992a107c0799a1b7b3
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-25 12:13:32 +0530

    switch statement

---
+++ Java_Introduction/_6_Switch_In_Java/Main.java
@@ -0,0 +1,43 @@
+package Java_Introduction._6_Switch_In_Java;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Enter the day number: ");
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+                break;
+        }
+
+        System.out.println("The day is " + dayName);
+
+        sc.close();
+    }
+}
