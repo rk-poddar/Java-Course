f689f1f3520fb19e3ceed3c3a9990052d54f4f14
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-25 12:38:47 +0530

    while loop

---
+++ Java_Introduction/_7_Loops_In_Java/_2_While_Loop/Main.java
@@ -0,0 +1,12 @@
+package Java_Introduction._7_Loops_In_Java._2_While_Loop;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int i = 0;
+        while (i < 5) {
+            System.out.println("The value of i is " + i);
+            i++;
+        }
+    }
+}
