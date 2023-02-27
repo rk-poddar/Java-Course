2f37ff66f8642ed7e0d794e5bb2a1cc167939095
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-27 15:26:15 +0530

    add pattern logic-6

---
+++ Pattern_Program_Logic/Logic_6/Main.java
@@ -0,0 +1,22 @@
+package Pattern_Program_Logic.Logic_6;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		int n=7;
+		
+		for(int i=0; i<n; i++){
+		    for(int j=0; j<n; j++){
+		        if(i==0 || j==0 || i==(n-1)/2 
+		        || i==n-1 || j==n-1 || j==(n-1)/2){
+		            System.out.print("* ");
+		        }
+		        else{
+		            System.out.print("  ");
+		        }
+		    }
+		    System.out.println();
+		}
+	}
+}
