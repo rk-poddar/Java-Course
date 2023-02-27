8f17d6b2a980985c51c8c1e95108b3b0346ac549
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-02-27 14:17:32 +0530

    single-dimensional array

---
+++ Java_Introduction/_10_Arrays_In_Java/_1_Single_Dimensional_Arrays/Main.java
@@ -0,0 +1,29 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int n[] = {1,2,3,4,5};
+	    
+	    for(int i=0; i<n.length; i++){
+	        System.out.print(n[i]+" ");
+	    }
+	    
+	    System.out.println();
+	    
+        // create an array of integers with 5 elements
+	    int a[] = new int [5];
+
+        // assign values to the array elements
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		a[4] = 5;
+		
+        // print the values of the array elements
+		for(int x : a){
+		    System.out.print(x+" ");
+		}
+		
+	}
+}
