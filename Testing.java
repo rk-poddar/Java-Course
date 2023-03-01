c4b1f9dd60dae2312e28581146e23c62eb984006
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-03-01 16:22:39 +0530

    Testing

---
+++ Testing.java
@@ -0,0 +1,140 @@
+import java.util.*;
+
+class Rectangle{
+
+    float length;
+    float breadth;
+    float area;
+
+    public void Input(){
+        System.out.println("Calculation of Rectangle :");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length: ");
+        length = sc.nextFloat();
+        System.out.print("Enter the breadth: ");
+        breadth = sc.nextFloat();
+    }
+
+    public void Compute(){
+        area = length * breadth;
+    }
+
+    public void Display(){
+        System.out.println("The area of a Rectangle is " + area);
+    }
+
+}
+
+class Square{
+
+    float length;
+    float area;
+
+    public void Input(){
+        System.out.println("Calculation of Square :");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length: ");
+        length = sc.nextFloat();
+    }
+
+    public void Compute(){
+        area = length * length;
+    }
+
+    public void Display(){
+        System.out.println("The area of a Square is " + area);
+    }
+
+}
+
+class Circle{
+
+    float radius;
+    float area;
+
+    public void Input(){
+        System.out.println("Calculation of Circle :");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius: ");
+        radius = sc.nextFloat();
+    }
+
+    public void Compute(){
+        area = 3.14f * radius * radius;
+    }
+
+    public void Display(){
+        System.out.println("The area of a Circle is " + area);
+    }
+
+}
+
+class Choices{
+    
+    public void Choice(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your choice : ");
+        System.out.println("1. Area of Rectangle ");
+        System.out.println("2. Area of Square ");
+        System.out.println("3. Area of Circle ");
+        int number = sc.nextInt();
+
+        switch(number){
+            case 1:
+                Rectangle re = new Rectangle();
+                re.Input();
+                re.Compute();
+                re.Display();
+            break;
+
+            case 2:
+                Square sq = new Square();
+                sq.Input();
+                sq.Compute();
+                sq.Display();
+            break;
+
+            case 3:
+                Circle cr = new Circle();
+                cr.Input();
+                cr.Compute();
+                cr.Display();
+            break;
+
+            default:
+                System.out.println("Invalid Input..! Try again...");
+                         
+        }  
+
+        sc.close();
+    }
+}
+
+
+public class Testing {
+    public static void main(String[] args) {
+
+        Choices ch = new Choices();
+        ch.Choice();
+        
+        System.out.println("You want more calculation: ");
+        System.out.println("1. Yes ");
+        System.out.println("2. No ");
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        switch(number){
+            case 1:
+                ch.Choice();
+            break;
+
+            case 2:
+                break;
+
+            default:
+                System.out.println("Wrong Input..");
+        }
+
+    }
+}
